f95adbb4b6e8b7733f0e73a24c03e48916929e28
BezawadaDivija <bezawadadivija15@gmail.com>
2025-05-26 23:41:40 +0530

    Create 6.data type demonstration.java

---
+++ Module3/corejava/6.data type demonstration.java
@@ -0,0 +1,17 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        // Declare and assign values to different primitive data types
+        int age = 25;
+        float height = 5.9f;
+        double weight = 70.45;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        // Display the values
+        System.out.println("Integer (int) value: " + age);
+        System.out.println("Float (float) value: " + height);
+        System.out.println("Double (double) value: " + weight);
+        System.out.println("Character (char) value: " + grade);
+        System.out.println("Boolean (boolean) value: " + isStudent);
+    }
+}
